deb41f2e3ef718d2b7f55f012bf2b38ab1cf4494
mattesanna017 <matteo.mog.sanna@gmail.com>
2022-07-21 13:25:57 +0100

    controllers

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const hbs = exphbs.create({});
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Describe what the following two lines of code are doing.
+
 // The following two lines of code are setting Handlebars.js as the default template engine.
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');

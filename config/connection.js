17682d846745f884ab48cb95bd6434c5a0efe93f
mattesanna017 <matteo.mog.sanna@gmail.com>
2022-07-24 16:54:48 +0100

    db clear

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,4 @@
-const Sequelize = require('sequelize');
+/*const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
@@ -11,5 +11,32 @@ const sequelize = new Sequelize(
     port: 3306,
   }
 );
+
+module.exports = sequelize;
+*/
+
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+let sequelize;
+
+sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+);
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 module.exports = sequelize;

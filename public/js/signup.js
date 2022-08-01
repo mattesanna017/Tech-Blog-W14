c2cdb0399a3a3a8ddb5954408fbea566e2ea52f0
mattesanna017 <matteo.mog.sanna@gmail.com>
2022-08-01 20:25:45 +0100

    minors

--- public/js/signup.js
+++ public/js/signup.js
@@ -3,8 +3,7 @@ const signupFormHandler = async (event) => {
 
     const username = document.querySelector('#username-input-signup').value.trim();
     const password = document.querySelector('#password-input-signup').value.trim();
-    // console.log(username);
-    // console.log(password);
+
     const response = await fetch('/api/user', {
         method: 'POST',
         body: JSON.stringify({
